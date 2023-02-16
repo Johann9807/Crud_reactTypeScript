137ecf54717eaff7810e0a6719d772fc03390c69
Johann.Casallas <Johann.Casallas@infotrack.com.co>
2023-02-16 15:44:56 -0500

    Se le aplican estilos al menu de navegacion con MaterialUI

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,24 +1,58 @@
 import React, { FunctionComponent } from 'react';
+import { AppBar, Toolbar, Box, Tabs, Tab, Button, Typography} from '@mui/material';
+import AutoStoriesIcon from '@mui/icons-material/AutoStories';
+
 interface IPropiedadesMenu{
-irAdministracionLibro:()=>void;
-irAdministracionUsuario:() => void; 
-irAdministracionPrestamo:() => void
+  irAdministracionLibro:()=>void;
+  irAdministracionUsuario:() => void; 
+  irAdministracionPrestamo:() => void
 }
 
-   export const Menu:FunctionComponent<IPropiedadesMenu> = ({irAdministracionLibro,irAdministracionUsuario,irAdministracionPrestamo}) =>{
+export const Menu: FunctionComponent<IPropiedadesMenu> = ({irAdministracionLibro, irAdministracionUsuario, irAdministracionPrestamo}) =>{
+  const [valorSeleccionado, setValorSeleccionado] = React.useState(0);
+
+  const cambiarValorSeleccionado = (evento: React.ChangeEvent<{}>, nuevoValor: number) => {
+    setValorSeleccionado(nuevoValor);
+  };
+
   return(
-    <div className="container">
-      <nav>
-      <h2 className="logo"><span>Book</span>shop</h2>
-      <ul>
-      <button className="button-menu" type="button" onClick={irAdministracionLibro}>Libros</button>
-      <button className="button-menu" type="button" onClick={irAdministracionUsuario}>Usuarios</button>
-      <button className="button-menu" type="button" onClick={irAdministracionPrestamo}>Prestamo</button>
-      </ul>
-      <button className="button-login" type="button">Login</button>
-      </nav>
-    </div>
-  )
+    <AppBar sx={{
+      height: 70, 
+      background: '#24303c' 
+      }}  
+      position="static">
+      <Toolbar>
+        <AutoStoriesIcon sx={{ 
+          display: { xs: 'none', md: 'flex' }, 
+          mr: 2, 
+          fontSize: 40
+        }} 
+          />
+        <Typography variant="h4" 
+        sx={{fontFamily: 'initial', 
+        }} 
+        style={{ 
+          color: '#fff' 
+          }}>
+          Bookshop
+        </Typography>
+        <Box ml="auto">
+          <Tabs sx={{paddingRight: 50}} textColor="inherit" value={valorSeleccionado} onChange={cambiarValorSeleccionado}>
+            <Tab label="Libros" onClick={irAdministracionLibro}/>
+            <Tab label="Usuarios" onClick={irAdministracionUsuario}/>
+            <Tab label="Prestamo" onClick={irAdministracionPrestamo}/>
+          </Tabs>
+        </Box>
+        <Button sx={{background: '#1f53c5'}} variant="contained" color="primary" className="button-login">
+            Login
+          </Button>
+      </Toolbar>
+    </AppBar>
+  );
 }
+
+
+
+
 
 
--- src/components/formularioLibros.tsx
+++ src/components/formularioLibros.tsx
@@ -1,81 +1,154 @@
 import React from 'react';
-import  { FunctionComponent, useState } from 'react';
+import { FunctionComponent, useState } from 'react';
 import ILibro from '../entidades/ILibro';
+import {
+  FormControl,
+  FormGroup,
+  FormLabel,
+  TextField,
+  Checkbox,
+  Button,
+  Typography,
+  Box
+} from '@mui/material';
+import { width } from '@mui/system';
 
 
 interface LibroProps {
-  alCambiarValor : (name : string, value : string) => void
-  guardarLibro : () => void
-  libro : ILibro
-  limpiarFormulario : () => void
+  alCambiarValor: (name: string, value: string) => void
+  guardarLibro: () => void
+  libro: ILibro
+  limpiarFormulario: () => void
 }
 
 
-const  FormularioLibros: FunctionComponent <LibroProps> = ({limpiarFormulario,alCambiarValor, guardarLibro,libro  }) => {
+const FormularioLibros: FunctionComponent<LibroProps> = ({ limpiarFormulario, alCambiarValor, guardarLibro, libro }) => {
   const handleSubmit = (event: { preventDefault: () => void; }) => {
-    event.preventDefault();  
-    guardarLibro() 
+    event.preventDefault();
+    guardarLibro()
     limpiarFormulario();
-    
+
   };
 
   const [isChecked, setIsChecked] = useState(false);
-  
-  
+
+
 
   return (
     <>
-    <form className='Form-libros' onSubmit={handleSubmit}>
-      <h4>Buscador Avanzado</h4>
-      <div>
-        <p className='ipt-check'><input  required type="checkbox" checked={isChecked} onChange={() => setIsChecked(!isChecked)} /> Acepta los terminos y condiciones</p>
-        
-        <label htmlFor="nombreLibro">Titulo: </label>
-        <input className="ipt-nombre" required disabled={!isChecked}
-          type="text"
-          id="nombreLibro"
-          name="NombreLibro"
-          value={libro.NombreLibro}
-          onChange={(event)=>alCambiarValor(event.target.name,event.target.value)} />
-      </div>
-      <div>
-        <label htmlFor="autorLibro">Autor: </label>
-        <input className="ipt-nombre"  required disabled={!isChecked}
-          type="text"
-          id="autorLibro"
-          name="AutorLibro"
-          value={libro.AutorLibro}
-          onChange={(event)=>alCambiarValor(event.target.name,event.target.value)} />
-      </div>
-      <div>
-      <label htmlFor="Editorial">Genero: </label>
-        <input className="ipt-nombre" required  disabled={!isChecked} minLength={5} maxLength={30}
-          type="text"
-          id="genero"
-          name="Genero"
-          value={libro.Genero}
-          onChange={(event)=>alCambiarValor(event.target.name,event.target.value)} />
-      </div>
-      <div>
-      <label htmlFor="editorial">Editorial: </label>
-        <input className="ipt-nombre" required disabled={!isChecked}
-          type="text"
-          id="editorial"
-          name="Editorial"
-          value={libro.Editorial}
-          onChange={(event)=>alCambiarValor(event.target.name,event.target.value)} />
-      </div>
-      <p>Para que la búsqueda sea más precisa rellene el máximo número de campos posibles.</p>
-      <div className='boton' >
-      <button type="submit">
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      Guardar</button>
-      
-      </div>
-    </form>
+      <Typography variant="h4" sx={{
+        textAlign: 'center',
+        paddingBottom: 3,
+        paddingTop: 9,
+        fontFamily: 'initial',
+        color: '#24303c'
+      }}>
+        Buscador Avanzado
+      </Typography>
+      <Box display="flex" justifyContent="center">
+        <FormControl component="form" className="Form-libros" onSubmit={handleSubmit}>
+          <FormGroup>
+            <FormLabel sx={{
+              paddingBottom: 3,
+              paddingTop: 1,
+              textAlign: 'center'
+            }}
+              htmlFor="aceptaTerminos">
+              <Checkbox
+                sx={{
+                  color: '#1f53c5'
+                }}
+                required
+                checked={isChecked}
+                onChange={() => setIsChecked(!isChecked)}
+                name="aceptaTerminos"
+                id="aceptaTerminos"
+              />Acepta los terminos y condiciones
+            </FormLabel>
+
+
+            <TextField
+              InputProps={{
+                style: {
+                  color: "#1f53c5",
+                  margin: "0 0 26px 0"
+                },
+              }}
+              required
+              disabled={!isChecked}
+              type="text"
+              id="nombreLibro"
+              name="NombreLibro"
+              label="Titulo"
+              value={libro.NombreLibro}
+              onChange={(event) => alCambiarValor(event.target.name, event.target.value)}
+            />
+
+            <TextField
+              InputProps={{
+                style: {
+                  color: "#1f53c5",
+                  margin: "0 0 26px 0"
+                },
+              }}
+              required
+              disabled={!isChecked}
+              type="text"
+              id="autorLibro"
+              name="AutorLibro"
+              label="Autor"
+              value={libro.AutorLibro}
+              onChange={(event) => alCambiarValor(event.target.name, event.target.value)}
+            />
+
+            <TextField
+              InputProps={{
+                style: {
+                  color: "#1f53c5",
+                  margin: "0 0 26px 0"
+                },
+              }}
+              required
+              disabled={!isChecked}
+              type="text"
+              id="genero"
+              name="Genero"
+              label="Genero"
+              value={libro.Genero}
+              onChange={(event) => alCambiarValor(event.target.name, event.target.value)}
+            />
+
+            <TextField
+              InputProps={{
+                style: {
+                  color: "#1f53c5",
+                  margin: "0 0 26px 0"
+                },
+              }}
+              required
+              disabled={!isChecked}
+              type="text"
+              id="editorial"
+              name="Editorial"
+              label="Editorial"
+              value={libro.Editorial}
+              onChange={(event) => alCambiarValor(event.target.name, event.target.value)}
+            />
+          </FormGroup>
+          <Typography 
+          variant="body1"
+          sx={{
+            paddingTop: 3,
+            paddingBottom: 5,
+          }}
+          >
+            Para que la búsqueda sea más precisa rellene el máximo número de campos posibles.
+          </Typography>
+          <Button variant="contained" color="primary" type="submit">
+            Guardar
+          </Button>
+        </FormControl>
+      </Box>
     </>
   );
 }
